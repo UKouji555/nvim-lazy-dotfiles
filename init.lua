8c09c97f4a495ffd72a95b58fa7d836b0c1e3c8a
UKouji555 <zerozero53205320.dev@gmail.com>
2025-06-01 02:34:02 +0900

    gitsignsキー操作追加

--- init.lua
+++ init.lua
@@ -1,6 +1,2 @@
 require("config.lazy")
 require("config.keymaps")
-
-vim.opt.encoding = "utf-8"
-vim.opt.fileencoding = "utf-8"
-vim.opt.ambiwidth = "double"
--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,5 +1,58 @@
 return {
   "lewis6991/gitsigns.nvim",
   event = { "BufReadPre", "BufNewFile" },
-  config = true,
+  config = function()
+    require("gitsigns").setup({
+      signs = {
+        add = { text = "+" },
+        change = { text = "~" },
+        delete = { text = "_" },
+        topdelete = { text = "‾" },
+        changedelete = { text = "~" },
+      },
+      current_line_blame = true, -- 現在行に blame を表示
+      on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        map("n", "]c", function()
+          if vim.wo.diff then
+            return "]c"
+          end
+          vim.schedule(function()
+            gs.next_hunk()
+          end)
+          return "<Ignore>"
+        end, { expr = true })
+
+        map("n", "[c", function()
+          if vim.wo.diff then
+            return "[c"
+          end
+          vim.schedule(function()
+            gs.prev_hunk()
+          end)
+          return "<Ignore>"
+        end, { expr = true })
+
+        map("n", "<leader>hs", gs.stage_hunk)
+        map("n", "<leader>hr", gs.reset_hunk)
+        map("n", "<leader>hb", gs.blame_line)
+      end,
+    })
+    vim.api.nvim_create_user_command("DiffWith", function(opts)
+      local ref = opts.args or "HEAD"
+      require("gitsigns").diffthis(ref)
+    end, {
+      nargs = 1,
+      complete = function()
+        return { "HEAD", "HEAD~1", "origin/main" }
+      end,
+    })
+  end,
 }
