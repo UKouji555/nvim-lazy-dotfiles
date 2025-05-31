b8a4ebb0019bcf8600d0444f9d3e629440132a72
UKouji555 <zerozero53205320.dev@gmail.com>
2025-06-01 04:13:34 +0900

    deeplのプラグイン追加

---
+++ lua/config/dotenv.lua
@@ -0,0 +1,16 @@
+local M = {}
+
+function M.eval(env_path)
+  local file = io.open(env_path, "r")
+  if not file then return end
+  for line in file:lines() do
+    local key, value = line:match("^([%w_]+)%s*=%s*(.+)$")
+    if key and value then
+      vim.env[key] = value
+    end
+  end
+  file:close()
+end
+
+return M
+
